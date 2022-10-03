3aae852e905db947e35c0d28d66af7e7a5cc3d14
Verna0214 <yuhsichen0214@gmail.com>
2022-10-03 17:47:29 +0800

    Add express static template

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send(`This is the restaurant list web.`)
+  res.render('index')
 })
 
 app.listen(port, () => {
